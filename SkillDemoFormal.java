ac7df906ade550ebc5ece7ce7a353230fe2a5128
KristinShuyiHan <97652556+KristinShuyiHan@users.noreply.github.com>
2022-02-04 10:47:24 -0800

    lib+testsss

--- SkillDemoFormal.java
+++ SkillDemoFormal.java
@@ -1,1 +1,20 @@
-public class SkillDemoFormal{}
+
+
+//This is my code from CSE8B PA 2
+
+public class SkillDemoFormal{
+
+    public static String repeat(char chara,int times){
+    
+        String stRing="";
+    
+        for (int initvalue=1;initvalue<=times;initvalue++){
+    
+            stRing= stRing+chara;
+    
+                }
+    
+        return stRing;
+    
+        }
+}
---
+++ SkillDemoFormalTest.java
@@ -0,0 +1,16 @@
+
+    
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class SkillDemoFormalTest{
+
+    @Test
+    public void repeatedString(){
+
+assertEquals("xxxx",SkillDemoFormal.repeat('x',2 )); //Failing Test 
+
+}
+
+    }  
